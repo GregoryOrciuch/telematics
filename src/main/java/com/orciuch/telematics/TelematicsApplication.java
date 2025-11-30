19a910553bd60aef09a95f9ef0270db86c82f248
Gregory Orciuch | evionica.com <g.orciuch@MacBook-Pro-Gregory.local>
2025-11-30 17:55:13 +0100

    Initial commit

---
+++ src/main/java/com/orciuch/telematics/TelematicsApplication.java
@@ -0,0 +1,13 @@
+package com.orciuch.telematics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelematicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelematicsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/orciuch/telematics/TelematicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orciuch.telematics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelematicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
